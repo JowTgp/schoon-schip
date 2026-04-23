470e8c65e5910a124dc3496853ca7dd58758d5ea
Jolina <jolina.arryn@student.ucll.be>
2026-04-23 12:04:22 +0200

    foutje html labels weg

--- oogst.js
+++ oogst.js
@@ -1,13 +1,13 @@
 export const basisBereidingen = [
 {
-    titel: 'tomatensaus',
+    titel: 'Tomatensaus',
     ingredienten: 'tomaat',
     techniek: 'bewaartechniek',
     moestuinlabel: ["tomaat"],
     bereiding: 'https://www.mooiemoestuin.nl/tomatensaus-2-0/'
 },
 {
-    titel: 'courgettesoep',
+    titel: 'Courgettesoep',
     ingredienten: 'courgette',
     techniek: 'recept',
     moestuinlabel: ["courgette"],
