63b8a1e2e96e30238133f12d06e44068f1d6a45c
Jolina <jolina.arryn@student.ucll.be>
2026-08-13 13:16:22 +0200

    recetpen

--- oogst.js
+++ oogst.js
@@ -7,18 +7,18 @@ export const basisBereidingen = [
     bereiding: 'https://www.mooiemoestuin.nl/tomatensaus-2-0/'
 },
 {
-    titel: 'Courgettesoep',
-    ingredienten: ["courgette"],
+    titel: 'Oosterse komkommersalade',
+    ingredienten: ["3 komkommers", "1 rode chilipeper", "50ml rijstazijn", "2tl honing", "2tl sesamzaadjes (zwart & wit licht geroosterd)", "scheutje sesamolie", "mispuntje chiliflakes", "zout"],
     techniek: 'recept',
-    moestuinlabel: ["courgette"],
-    bereiding: 'kook de courgette'
+    moestuinlabel: ["komkommer", "chilipeper"],
+    bereiding: "Komkommer snijden naar keuze: dunne plakjes of blokjes. Rijstazijn, honing en sesamolie mengen. Chilipeper fijn snijden. Alles mengen behalve de sesamzaadjes en even in de frigo laten marineren. Sesamzaadjes erover strooien vlak voor het serveren."
 },
 {
     titel: 'Udonnoedels met rodekool en bloemkool',
     ingredienten: ["500gr rode kool", "1 kleine bloemkool", "olie", "4el sojasaus", "8-10 bosuien", "2 tenen knoflook", "600gr kant-en-klare udonnoedels", "1tl kerriepoeder", "2el mirin", "2el limoensap", "20gr ingelegde gember", "sesamzaad"],
     techniek: 'recept',
     moestuinlabel: ["rodekool", "bloemkool"],
-    bereiding: "Zie kookboek 'Azië' van Meera Sodha pg. 88-89."
+    bereiding: "Zie 'Azië' pg. 88-89 van Meera Sodha."
 },
 {
     titel: 'Kappertjes van Oost-Indische Kers',
@@ -32,22 +32,79 @@ export const basisBereidingen = [
     ingredienten: ["30gr sesamzaad", "150gr sperziebonen", "1/2e witte kool (400gr)", "1 ajuin", "2 citroenen", "2 tomaten", "1 bosje koriander", "3el olijfolie", "peper & zout"],
     techniek: 'recept',
     moestuinlabel: ["sperzieboon", "wittekool", "ajuin", "tomaat"],
-    bereiding: "Zie kookboek 'Grieks! Recepten voor elke dag' van Georgina Hayden pg. 198-199."
+    bereiding: "Zie 'Grieks!' pg. 198-199 van Georgina Hayden."
+},
+{
+    titel: 'Bietjes uit de oven met dilleyoghurt',
+    ingredienten: ["1kg bietjes met blad", "olijfolie", "peper & zout", "6 tenen knoflook", "1el rodewijnazijn", "1/2 bosje dille", "250gr Griekse yoghurt", "1 citroen", "2tl kappertjes"],
+    techniek: 'recept',
+    moestuinlabel: ["rodebiet"],
+    bereiding: "Zie 'Cyprus' pg. 69 van Georgina Hayden."
 },
 {
     titel: 'Tomaten met sumak-uien en pijnboompitten',
     ingredienten: ["1 grote sjalot (70gr)", "1+1/2 el sumak", "2tl witte wijnazijn", "2el olijfolie", "700gr gemengde tomaten (groen, geel, rood en kerstomaatjes)", "15gr verse basilicum", "25gr pijnboompitten", "peper & zout"],
     techniek: 'recept',
     moestuinlabel: ["tomaat", "sjalot"],
-    bereiding: "Zie kookboek 'Simpel' van Ottolenghi pg. 34-35."
+    bereiding: "Zie 'Simpel' pg. 34-35 van Ottolenghi."
 },
 {
-    titel: 'Tomaten, snijbiet en spinazie met greoosterde amandelen',
-    ingredienten: ["50gr geschaafde amandelen", "1/2 tl paprikapoeder", "1+1/2 tl karwijzaad", "60ml olijfolie", "800gr romatomaten", "500gr snijbiet", "130gr spinazie", "2 limoenen", "35gr munt", "35gr dille", "8bosuien", "zout"],
+    titel: 'Tomaten, snijbiet en spinazie met geroosterde amandelen',
+    ingredienten: ["50gr geschaafde amandelen", "1/2 tl paprikapoeder", "1+1/2 tl karwijzaad", "60ml olijfolie", "800gr romatomaten", "500gr snijbiet", "130gr spinazie", "2 limoenen", "35gr munt", "35gr dille", "8 bosuien", "zout"],
     techniek: 'recept',
     moestuinlabel: ["tomaat", "snijbiet", "spinazie"],
-    bereiding: "Zie kookboek 'Simpel' van Ottolenghi pg. 72-73."
-}
+    bereiding: "Zie 'Simpel' pg. 72-73 van Ottolenghi."
+},
+{
+    titel: 'Melanzane alla tarantina',
+    ingredienten: ["2 aubergines", "zout", "gehakte zwarte olijven", "kappertjes", "geraspte pecorino romano of parmezaan", "verse munt, gehakt", "verse bailicum, gehakt", "olijfolie"],
+    techniek: 'recept',
+    moestuinlabel: ["aubergine"],
+    bereiding: 'Oven voorverwarmen 200°. Aubergines overdwars snijden en 30 min in lauwwarm water weken met zout, laat uilekken en droog deppen. Aubergines insnijden, kruisling niet volledig erdoor. De olijven, kaas, kappertjes en kruiden mengen en smeer de mengeling op de aubergine-helften, duw ook een beetje mengsel in de sneden. Met olijolie overgieten en in de overnplaatsen 30-40min (afhankelijk van de grootte van de aubergines).'
+},
+{
+    titel: 'Erwten of sperziebonen in tomatensaus',
+    ingredienten: ["600gr aardappelen", "olijfolie", "2 ajuinen", "3 wortels", "5 rijpe tomaten of 1 blik 400gr tomaten", "1el tomatenpuree", "500ml plantaardige bouillon", "600gr sperziebonen of 900gr erwten", "4 artisjokharten"],
+    techniek: 'recept',
+    moestuinlabel: ["aardappel", "ajuin", "wortel", "tomaat", "peulen", "sperzieboon"],
+    bereiding: "Zie 'Cyprus' pg. 98-99 van Georgina Hayden. Zie inleiding voor bijgerechtjes."
+},
+{
+    titel: 'Cornribs met misoboter en shichimi togarishi',
+    ingredienten: ["1 maiskolf (rauw/vers)", "30 gr boter", "1 limoen", "2 tl honing", "1 teentje look", "2 tl shichimi togarishi", "1 tl witte miso", "1 el sojasaus", "1 lente-uitje", "1 el (Japanse) mayonaise"],
+    techniek: 'recept',
+    moestuinlabel: ["suikermais"],
+    bereiding: 'https://www.avocadovandeduivel.be/recepten/cornribs-met-misoboter-en-shichimi-togarashi/'
+},
+{
+    titel: 'Uien in miso-boter',
+    ingredienten: ["8 middelgrote uien", "100 gr boter", "100 gr witte miso", "1 liter warm water"],
+    techniek: 'recept',
+    moestuinlabel: ["ajuin"],
+    bereiding: 'https://www.watatenzij.nl/2021/01/22/uien-in-miso-boter/'
+},
+{
+    titel: 'Rucolasalade met mozarella',
+    ingredienten: ["1/2 Chartenais of andere Cantapoupe meloen", "75 gr rucola", "150 gr rauwe ham", "125gr mozarella", "4el balsamicocreme", "3el olijfolie", "50gr walnoten, gehakt"],
+    techniek: 'recept',
+    moestuinlabel: ["meloen", "rucola"],
+    bereiding: 'Meloen in stukken snijden. Rucola en meloen verdelen over de borden met de ham en mozarella in stukken gescheurd erover. Besprenkel met de wlanoten stukjes, olie en balsamico. Variaties: 1) Vervang de walnoten door lichtgeroosterde amandelschilfers en/of pijnboompitten. 2) Leg de ham even in de over zodat die krokant wordt, strooi hem als een crumble over de salade. 3) Vervang de meloen door geroosterde necatrine.'
+},
+{
+    titel: 'Carrot cake',
+    ingredienten: ["3 eieren", "180 ml zonnebloemolie", "150 gr lichtbruine bastardsuiker", "1.2 tl vanilleextract", "180gr zelfrijzend bakmeel", "snufje zout", "1.2 tl kaneel", "240gr wortelen", "75gr walnoten", "boter", "240gr roomkaas", "90 gr bloemsuiker"],
+    techniek: 'recept',
+    moestuinlabel: ["wortel"],
+    bereiding: 'https://www.leukerecepten.nl/recepten/carrot-cake/'
+},
+{
+    titel: 'Venkel-pompelmoes-salade',
+    ingredienten: ["1 pompelmoes", "1 venkel", "100 gr feta", "verse sla","walnoten of pecannoten, gehakt", "olijfolie", "honing", "peper"],
+    techniek: 'recept',
+    moestuinlabel: ["venkel", "rucola"],
+    bereiding: 'Pompelmoes schillen en sap opvangen. Venkel in dunne reepjes snijden. Meng het pompelmoessap, olijfolie en honing en laat de venkel er even in marineren. Meng de sla, pomelmoes en venkel. Bestrooi me feta en walnoten, werk af met peper.'
+},
+
 ]; 
 
 
